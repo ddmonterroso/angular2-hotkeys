1335c6efd49f8e0f23d40294623569f369c2e5af
ddmonterroso <ddmonterroso@gmail.com>
2020-10-27 15:16:02 +0100

    Update hotkeys.directive.ts

--- src/hotkeys.directive.ts
+++ src/hotkeys.directive.ts
@@ -3,6 +3,7 @@ import {Hotkey, ExtendedKeyboardEvent} from './hotkey.model';
 import {HotkeysService} from './hotkeys.service';
 //import 'mousetrap';
 import { MousetrapInstance } from 'mousetrap';
+import * as Mousetrap from 'mousetrap';
 
 @Directive({
     selector : '[hotkeys]',
